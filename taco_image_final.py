79744929a61feed17e3fecd37c2c618c5d2b60e7
frostbite169 <orlok961@gmail.com>
2019-12-06 17:23:04 -0600

    adding comments

--- taco_image_final.py
+++ taco_image_final.py
@@ -1,8 +1,8 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont   #importing what I need to be able to do what I want with picture
 
-image = Image.open('taco.jpg')
-bigger = image.resize((333, 500))
-img_draw = ImageDraw.Draw(bigger)
-font = ImageFont.truetype('DejaVuSans.ttf', 25)
-img_draw.text([20, 0], 'Random Taco Cookbook', fill='purple', font=font)
-bigger.save('resizedTaco.jpg')
+image = Image.open('taco.jpg')   #making it so that i can change the picture of taco.jpg
+bigger = image.resize((233, 350))   #creating a resized version of taco.jpg
+img_draw = ImageDraw.Draw(bigger)   #creating a version of bigger that I can draw on
+font = ImageFont.truetype('DejaVuSans.ttf', 15)   #creating a variable for the font I want to write with
+img_draw.text([20, 0], 'Random Taco Cookbook', fill='purple', font=font)    #writing random taco cookbool in purple with the font I put in the variable
+bigger.save('resizedTaco.jpg')   #saving the version of taco.jpg I altered
